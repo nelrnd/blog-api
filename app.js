9381f9f667d139b34507f985dbdd77ee9aa202a2
Nel <nel.renaudin@gmail.com>
2023-12-03 17:43:48 +0100

    Setup basic Express app and connect to database

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Connect to DB
+const mongoDB = process.env.MONGODB_URI
+const main = async () => mongoose.connect(mongoDB)
+main().catch((err) => console.error(err))
+
+app.get("/", (req, res) => res.send("Hello World!"))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
