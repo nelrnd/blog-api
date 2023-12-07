860b2b56c137be693cb1d7a96f2263a990d1c8f3
Nel <nel.renaudin@gmail.com>
2023-12-07 17:19:49 +0100

    Enable CORS

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 // Connect to DB
 const mongoDB = process.env.MONGODB_URI
